ff58510ac6b5bb212c97755736a5f159fd1c8e6c
habib01cse <habib01cse@gmail.com>
2021-07-14 20:01:58 +0600

    Implementation ng-bootstrap and create routing module and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './layout/layout.module';
+import { PromiseComponent } from './promise/promise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromiseComponent
   ],
   imports: [
     BrowserModule
+    , LayoutModule
+    , AppRoutingModule
+    , NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
