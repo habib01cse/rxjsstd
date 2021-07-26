43413735b770c830e37bab6adc890e8ccaf4ed60
habib01cse <habib01cse@gmail.com>
2021-07-26 21:09:12 +0600

    Explore rxjs toArray operators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ListComponent } from './observable/list/list.component';
 import { FromEventComponent } from './observable/from-event/from-event.component';
 import { IntervalComponent } from './observable/interval/interval.component';
 import { OfFromComponent } from './observable/of-from/of-from.component';
+import { ToArrayComponent } from './observable/to-array/to-array.component';
 
 const routes: Routes = [
   { 
@@ -32,6 +33,7 @@ const routes: Routes = [
       {path: 'fromEvent', component: FromEventComponent},
       {path: 'interval', component: IntervalComponent},
       {path: 'of-from', component: OfFromComponent},
+      {path: 'to-array', component: ToArrayComponent},
     ]
   },
   {
